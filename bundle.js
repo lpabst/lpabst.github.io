f293d6c1466810da38bea8a10ce69b1ca90a41c2
loren pabst <loren pabst>
2017-05-08 12:14:46 -0600

    added greensock button

--- bundle.js
+++ bundle.js
@@ -24,6 +24,30 @@ angular.module('myApp', ['ui.router']).config(["$urlRouterProvider", "$stateProv
 }]);
 'use strict';
 
+$(document).ready(function () {
+
+    var $box = $('#box');
+
+    $box.mouseenter(function () {
+        var randX = Math.floor(Math.random() * 20 + 1);
+        var randY = Math.floor(Math.random() * 800 + 1);
+
+        TweenLite.to($box, 1, { left: randX, x: 0, top: randY });
+    });
+
+    $box.click(function () {
+        var randX = Math.floor(Math.random() * 600 + 1);
+        var randY = Math.floor(Math.random() * 200 + 1);
+
+        $box.text('Not This Time Sucka!');
+        TweenLite.to($box, 1, { left: randX, top: randY });
+        setTimeout(function () {
+            $box.text('Leave Neg Feedback');
+        }, 1500);
+    });
+});
+'use strict';
+
 angular.module('myApp').controller('mainCtrl', ["$scope", "movieService", function ($scope, movieService) {
 
   $scope.mobileMenu = false;
@@ -124,6 +148,25 @@ angular.module('myApp').controller('movieSearchCtrl', ["$scope", "movieService",
 }]);
 'use strict';
 
+angular.module('myApp').controller('specificMovieCtrl', ["$scope", "movieService", function ($scope, movieService) {
+
+    $scope.movieHasBeenSearched = false;
+
+    $scope.showSpecificMovie = function (movie_name) {
+
+        movieService.showSpecificMovie(movie_name).then(function (response) {
+            $scope.title = response.data.Title;
+            $scope.rating = response.data.Rated;
+            $scope.runtime = response.data.Runtime;
+            $scope.revenue = response.data.BoxOffice;
+            $scope.reviewRating = response.data.imdbRating;
+            $scope.searchByName = '';
+            $scope.movieHasBeenSearched = true;
+        });
+    };
+}]);
+'use strict';
+
 angular.module('myApp').controller('quotesCtrl', ["$scope", "quotesService", function ($scope, quotesService) {
 
     $scope.getQuote = function () {
@@ -143,24 +186,5 @@ angular.module('myApp').service('quotesService', ["$http", function ($http) {
 
     this.getQuote = function () {
         return $http.get('http://quotes.rest/qod.json');
-    };
-}]);
-'use strict';
-
-angular.module('myApp').controller('specificMovieCtrl', ["$scope", "movieService", function ($scope, movieService) {
-
-    $scope.movieHasBeenSearched = false;
-
-    $scope.showSpecificMovie = function (movie_name) {
-
-        movieService.showSpecificMovie(movie_name).then(function (response) {
-            $scope.title = response.data.Title;
-            $scope.rating = response.data.Rated;
-            $scope.runtime = response.data.Runtime;
-            $scope.revenue = response.data.BoxOffice;
-            $scope.reviewRating = response.data.imdbRating;
-            $scope.searchByName = '';
-            $scope.movieHasBeenSearched = true;
-        });
     };
 }]);
